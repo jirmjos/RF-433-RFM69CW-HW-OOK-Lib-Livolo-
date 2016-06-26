d2e0d1c4bc43fa1b34d787e3792b56671ed66650
Alexey P <alexey@avp.name>
2016-06-26 15:19:18 +0300

    add serializable

---
+++ libutils/Serializable.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Serializable.h"
+<<<<<<< HEAD
+#include "logging.h"
+=======
+#include "../libs/libutils/logging.h"
+>>>>>>> a0862dc2f40504a926963571d709c484fe28b8fb
+
+
+void CSerializable::Dump(CLog *log)
+{
+	log->Printf(1, "CSerializable::Dump");
+}
